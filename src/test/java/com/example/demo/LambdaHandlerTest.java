7a107c51342f6ecd60c1c929cd1c4868225b0075
Mahefaa <hei.mahefa@gmail.com>
2025-07-09 17:28:58 +0300

    chore: just run the spring boot app

--- src/test/java/com/example/demo/LambdaHandlerTest.java
+++ src/test/java/com/example/demo/LambdaHandlerTest.java
@@ -11,22 +11,22 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class LambdaHandlerTest {
-    LambdaHandler handler = new LambdaHandler();
+  LambdaHandler handler = new LambdaHandler();
 
-    @Test
-    void handle_non_vaadin_event_ok() throws IOException {
-        LambdaUrlRequestEvent event = new LambdaUrlRequestEvent();
-        RequestContext requestContext = new RequestContext();
-        Http http = new Http();
-        http.setMethod("GET");
-        http.setPath("/ping");
-        requestContext.setHttp(http);
-        event.setRequestContext(requestContext);
-        event.setHeaders(Map.of());
-        event.setVersion("1.0");
-        event.setRawPath("/ping");
-        var response = handler.handleRequest(event, null);
+  @Test
+  void handle_non_vaadin_event_ok() throws IOException {
+    LambdaUrlRequestEvent event = new LambdaUrlRequestEvent();
+    RequestContext requestContext = new RequestContext();
+    Http http = new Http();
+    http.setMethod("GET");
+    http.setPath("/ping");
+    requestContext.setHttp(http);
+    event.setRequestContext(requestContext);
+    event.setHeaders(Map.of());
+    event.setVersion("1.0");
+    event.setRawPath("/ping");
+    var response = handler.handleRequest(event, null);
 
-        assertEquals("pong", response.getBody());
-    }
+    assertEquals("working", response.getBody());
+  }
 }
